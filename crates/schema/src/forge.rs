983f08dd590f01c1ac71d7ba390373704542f75d
Moulberry <james.jenour@protonmail.com>
2026-06-28 23:53:27 +0800

    Make versions public again

--- crates/schema/src/forge.rs
+++ crates/schema/src/forge.rs
@@ -164,7 +164,7 @@ impl LegacyVersionInfo {
 }
 
 #[derive(Debug)]
-pub struct ForgeMavenManifest(pub(crate) Vec<Ustr>);
+pub struct ForgeMavenManifest(pub Vec<Ustr>);
 
 #[derive(Debug)]
-pub struct NeoforgeMavenManifest(pub(crate) Vec<Ustr>);
+pub struct NeoforgeMavenManifest(pub Vec<Ustr>);
